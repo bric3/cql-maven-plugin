766f425d3f86574b4cf324f9f22e0fa5b95999c0
Brice Dutheil <brice.dutheil@gmail.com>
2016-05-13 12:18:35 +0200

    Moves deployment plugin in dedicated profile

---
+++ maven-central-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Deploy maven artifact in current directory into Maven central repository
+# using maven-release-plugin goals
+
+read -p "Really deploy to maven central repository  (yes/no)? "
+[ "$REPLY" != "yes" ] && echo "Didn't deploy anything" && exit 1
+
+echo mvn release:clean release:prepare release:perform -B -e | tee maven-central-deploy.log
