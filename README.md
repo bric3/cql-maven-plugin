5e8fec42002c4edcc000ee8722492ce4836b6e36
Brice Dutheil <brice.dutheil@gmail.com>
2016-10-06 15:11:53 +0200

    Adds support for Cassandra 3.0 with driver 3.1.0
    Updated docs accordingly. Added configurable read timeout.

--- README.md
+++ README.md
@@ -1,48 +1,15 @@
 # cql-maven-plugin
 
-[![Build Status](https://travis-ci.org/bric3/cql-maven-plugin.svg)](https://travis-ci.org/bric3/cql-maven-plugin) [![Apache License](http://img.shields.io/badge/license-Apache-blue.svg) ](https://github.com/bric3/cql-maven-plugin/blob/master/LICENSE)
+[![Build Status](https://travis-ci.org/bric3/cql-maven-plugin.svg)](https://travis-ci.org/bric3/cql-maven-plugin) [![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.bric3.maven/cql-maven-plugin/badge.svg?style=flat)](https://maven-badges.herokuapp.com/maven-central/com.github.bric3.maven/cql-maven-plugin) [![Apache License](http://img.shields.io/badge/license-Apache-blue.svg) ](https://github.com/bric3/cql-maven-plugin/blob/master/LICENSE)
 
 
 This plugin allows one to execute CQL statements on a cassandra cluster.
-
-**Note this plugin is not deployed on any public maven repo, at the moment it is necessary to deploy it on a privately owned maven repository.**
-
-### To install locally :
-
-```bash
-mvn clean install
-```
-
-### To deploy a private maven repo :
-
-Assuming the `pom.xml` is patched with a `distributionManagement` element like 
-
-```xml
-    <distributionManagement>
-        <repository>
-            <id>our-thirdparty</id>
-            <name>Our Third Party Repository</name>
-            <url>https://host/nexus/content/repositories/thirdparty/</url>
-        </repository>
-    </distributionManagement>
-```
-
-Deploy it with the following command line : 
-
-```bash
-mvn versions:set -DnewVersion=0.1
-git commit --all --message="Version 0.1"
-mvn deploy scm:tag
-```
-
-As this plugin is not released on central, it would be preferable to use a suffix to the version it avoid possible collision if this project ever get published on central. That means that version `0.1-myproject` should be used instead of a _raw_ `0.1`.
-
 
 ## Requirements
 
  * Java 8
  * Maven 3.3.3 (maybe older version work, but this has not been tested)
- * Cassandra 2.0 to 2.1 (driver is 2.1.6)
+ * Apache Cassandra 1.2, 2.0, 2.1, 2.2 or 3.0 (driver is 3.1.0, see [driver compatibility matrix](http://docs.datastax.com/en/developer/java-driver//3.1/manual/native_protocol/))
 
 ## Configuration
 
@@ -66,10 +33,52 @@ This plugin has a single goal (`execute`) and runs by default in the `pre-integr
 
 ### Example
 
+If you have CQL files like (note the semi-column `;` otherwise statements are not correctly identified) :
+
+* ```cql
+  DROP KEYSPACE IF EXISTS my_keyspace;
+  ```
+
+* ```cql
+  CREATE KEYSPACE my_keyspace 
+  WITH replication = { 
+      'class':'NetworkTopologyStrategy', 
+      'dc1': 1} 
+  AND durable_writes = 'false';
+  ```
+
+* ```cql
+  CREATE TABLE user_credentials (
+     email text,
+     password text,
+     userid uuid,
+     PRIMARY KEY (email)
+  );
+  
+  CREATE TABLE users (
+     userid uuid,
+     firstname varchar,
+     lastname varchar,
+     email text,
+     created_date timestamp,
+     PRIMARY KEY (userid)
+  );
+  
+  CREATE TYPE video_metadata (
+     height int,
+     width int,
+     video_bit_rate set<text>,
+     encoding text
+  );
+  ``` 
+
+And you have the following plugin definition :
+
 ```xml
 <plugin>
     <groupId>com.github.bric3</groupId>
     <artifactId>cql-maven-plugin</artifactId>
+    <version>0.3</version>
     <configuration>
         <username>ajax</username>
         <password>******************</password>
@@ -122,4 +131,44 @@ This plugin has a single goal (`execute`) and runs by default in the `pre-integr
 </plugin>
 
 ```
+
+
+### Notes
+
+* The driver may warn users when a name returns multiple IP addresses, due to a change in 3.0.0. See : [JAVA-975](https://datastax-oss.atlassian.net/browse/JAVA-975)
+
+  > §13. If a DNS name resolves to multiple A-records, `Cluster.Builder#addContactPoint(String)` will now use all of these addresses as contact points. This gives you the possibility of maintaining contact points in DNS configuration, and having a single, static contact point in your Java code.
+
+
+## For cql-maven-plugin developers 
+
+### To install locally :
+
+```bash
+mvn clean install
+```
+
+### To deploy a private maven repo :
+
+Assuming the `pom.xml` is patched with a `distributionManagement` element like 
+
+```xml
+    <distributionManagement>
+        <repository>
+            <id>our-thirdparty</id>
+            <name>Our Third Party Repository</name>
+            <url>https://host/nexus/content/repositories/thirdparty/</url>
+        </repository>
+    </distributionManagement>
+```
+
+Deploy it with the following command line : 
+
+```bash
+mvn versions:set -DnewVersion=0.1
+git commit --all --message="Version 0.1"
+mvn deploy scm:tag
+```
+
+This plugin is released on central, but if crafting your own version it would be preferable to use a suffix to the version to avoid possible collision with an the coordinate of an artifact deployed on central. That means that version `0.1-myproject` should be used instead of a _raw_ `0.1`.
 
--- src/main/java/com/github/bric3/CqlExecuteMojo.java
+++ src/main/java/com/github/bric3/CqlExecuteMojo.java
@@ -1,8 +1,18 @@
 package com.github.bric3;
 
-import static java.nio.file.Files.readAllBytes;
-import static java.util.stream.Collectors.joining;
-import static org.apache.maven.plugins.annotations.LifecyclePhase.PRE_INTEGRATION_TEST;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SocketOptions;
+import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+import com.google.common.base.Stopwatch;
+import org.apache.maven.model.FileSet;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -11,26 +21,21 @@ import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
-import org.apache.maven.model.FileSet;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.codehaus.plexus.util.FileUtils;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.SocketOptions;
-import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
+
+import static com.datastax.driver.core.SocketOptions.DEFAULT_READ_TIMEOUT_MILLIS;
+import static java.nio.file.Files.readAllBytes;
+import static java.util.stream.Collectors.joining;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.PRE_INTEGRATION_TEST;
 
 /**
  * Executes CQL statements;
  */
 @Mojo(name = "execute",
-        defaultPhase = PRE_INTEGRATION_TEST,
-        threadSafe = true)
+      defaultPhase = PRE_INTEGRATION_TEST,
+      threadSafe = true)
 public class CqlExecuteMojo extends AbstractMojo {
 
+    public static final Pattern STATEMENT_END = Pattern.compile(";");
     /**
      * Skip the execution.
      */
@@ -91,6 +96,13 @@ public class CqlExecuteMojo extends AbstractMojo {
     @Parameter(property = "cassandra.logStatements", defaultValue = "false")
     private boolean logStatements = false;
 
+    /**
+     * Default timeout longer than usual because this plugin may execute rare but long running
+     * statements like dropping keyspace. The execution can be even longer when the node is running on a VM.
+     */
+    @Parameter(property = "cassandra.readTimeoutMillis")
+    private int readTimeoutMillis = DEFAULT_READ_TIMEOUT_MILLIS * 4;
+
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (skip) {
@@ -112,7 +124,6 @@ public class CqlExecuteMojo extends AbstractMojo {
         } catch (Exception e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
-
     }
     
     Session connectTo(Cluster cluster) {
@@ -123,17 +134,15 @@ public class CqlExecuteMojo extends AbstractMojo {
     }
 
     private void logStatement(String statement) {
-        if(logStatements) {
-            getLog().info("Applying : '" + statement.replaceAll("[\\t\\r\\n ]+", " ") + "'");
+        if (logStatements) {
+            getLog().info("Applying : '" + statement.replaceAll("[\\t\\r\\n ]+", " ") + ";'");
         }
     }
 
     private Stream<String> toStatements(String content) {
-
-        return Pattern.compile(";")
-                      .splitAsStream(content)
-                      .map(String::trim)
-                      .filter((s) -> !s.isEmpty());
+        return STATEMENT_END.splitAsStream(content)
+                            .map(String::trim)
+                            .filter((s) -> !s.isEmpty());
     }
 
     private Stream<String> readContent(File file) {
@@ -151,7 +160,7 @@ public class CqlExecuteMojo extends AbstractMojo {
 
     private Stream<String> onMissingFile(File file) {
         if (ignoreMissingFile) {
-            getLog().error("Specified script " + file.getAbsolutePath() + " does not exist. Ignoring as loadFailureIgnore is true");
+            getLog().error("Specified script " + file.getAbsolutePath() + " does not exist. Ignoring as 'ignoreMissingFile' is true");
             return Stream.empty();
         } else {
             throw new RuntimeException(new MojoFailureException("Specified script " + file.getAbsolutePath() + " does not exist."));
@@ -159,13 +168,14 @@ public class CqlExecuteMojo extends AbstractMojo {
     }
 
     private Cluster cluster() {
-        SocketOptions socketOptions = new SocketOptions();
-        socketOptions.setKeepAlive(true);
-
-        return Cluster.builder().addContactPoints(contactPoint).withPort(port)
+        return Cluster.builder()
+                      .addContactPoints(contactPoint).withPort(port)
                       .withCredentials(username, password)
-                      .withSocketOptions(socketOptions)
-                      .withLoadBalancingPolicy(new DCAwareRoundRobinPolicy(localDatacenter))
+                      .withSocketOptions(new SocketOptions().setKeepAlive(true)
+                                                            .setReadTimeoutMillis(readTimeoutMillis))
+                      .withLoadBalancingPolicy(DCAwareRoundRobinPolicy.builder()
+                                                                      .withLocalDc(localDatacenter)
+                                                                      .build())
                       .build();
     }
 
@@ -190,10 +200,16 @@ public class CqlExecuteMojo extends AbstractMojo {
         }
 
         public void execute(String statement) {
+            Stopwatch watch = Stopwatch.createUnstarted();
             try {
+                watch.start();
                 session.execute(statement);
             } catch (Exception e) {
-                CqlExecuteMojo.this.getLog().error("Failing statement '" + statement + "'", e);
+                getLog().error("Failing statement '" + statement + "'", e);
+            } finally {
+                if (logStatements) {
+                    getLog().info("Statement took : " + watch);
+                }
             }
         }
     }
